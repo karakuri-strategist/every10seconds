7cecee8fbf700b24a8d4b3f83eca5a7121c4285d
karakuri-strategist <alecfeatherston@gmail.com>
2022-10-10 21:49:49 -0700

    add shark

--- Assets/Scripts/Sentinel.cs
+++ Assets/Scripts/Sentinel.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Sentinel : MonoBehaviour
 {
+    public delegate void Spotted(bool spotted, Sentinel sender);
+    public event Spotted PlayerSpotted;
+
     public float rotateSpeed = 3;
     public float chaseRotateSpeed = 6;
     [SerializeField]
@@ -30,23 +33,23 @@ public class Sentinel : MonoBehaviour
             float angle = Vector3.SignedAngle(targetDireciton, transform.forward, Vector3.up);
             if(Mathf.Abs(angle) < spotlight.spotAngle && targetDireciton.magnitude < spotlight.range)
             {
-                Debug.Log(angle);
                 RaycastHit hit;
                 if (Physics.Raycast(transform.position, player.position - transform.position, out hit))
                 {
                     if (hit.collider.tag == "Player")
                     {
                         transform.Rotate(Vector3.up * Time.deltaTime * -1 * Mathf.Sign(angle) * chaseRotateSpeed, Space.World);
-                        Debug.Log("Gotcha!");
+                        PlayerSpotted.Invoke(true, this);
                     } else
                     {
                         transform.Rotate(Vector3.up * Time.deltaTime * rotateDirection * rotateSpeed, Space.World);
+                        PlayerSpotted.Invoke(false, this);
                     }
                 }
             } else
             {
                 transform.Rotate(Vector3.up * Time.deltaTime * rotateDirection * rotateSpeed, Space.World);
-
+                PlayerSpotted.Invoke(false, this);
             }
         }
     }
@@ -57,6 +60,8 @@ public class Sentinel : MonoBehaviour
         on = !on;
         spotlight.enabled = on;
         lightBulb.sharedMaterial = on ? lightBulbGlow : lightBulbOff;
+        if (!on)
+            PlayerSpotted.Invoke(false, this);
         StartCoroutine(Toggle());
     }
 }
---
+++ Assets/Scripts/Shark.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Shark : MonoBehaviour
+{
+    public float wanderRadius;
+    public float wanderTimer;
+    public float wanderSpeed = 3.5f;
+    public float chaseSpeed = 7f;
+    public Transform player;
+
+    private NavMeshAgent agent;
+    private float timer;
+    private Vector3 destination;
+    private bool alerted = false;
+
+    // Use this for initialization
+    void OnEnable()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        timer = wanderTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(alerted)
+        {
+            agent.SetDestination(player.position);
+            agent.speed = chaseSpeed;
+        } else
+        {
+            agent.speed = wanderSpeed;
+            if (timer >= wanderTimer)
+            {
+                Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+                destination = newPos;
+                timer = 0;
+            }
+            agent.SetDestination(Vector3.Lerp(agent.destination, destination, 0.5f * Time.deltaTime));
+        }
+        transform.rotation = Quaternion.LookRotation(agent.velocity.normalized) * Quaternion.AngleAxis(90, Vector3.up);
+    }
+
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere.normalized * dist;
+
+        randDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
+    }
+
+    public void OnSpotted(bool spotted)
+    {
+        alerted = spotted;
+    }
+}
